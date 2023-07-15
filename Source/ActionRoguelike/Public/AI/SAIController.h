06df64b1e6afcbbcc13e90cd2ad72024e23fedc9
kshestakova <71833659+kshestakova@users.noreply.github.com>
2023-07-15 20:22:30 +0300

    Added AI behavior

---
+++ Source/ActionRoguelike/Public/AI/SAIController.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "SAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONROGUELIKE_API ASAIController : public AAIController
+{
+	GENERATED_BODY()
+		
+	UPROPERTY(EditDefaultsOnly, Category = "AI")
+	UBehaviorTree* BehaviorTree;
+
+	virtual void BeginPlay() override;
+};
